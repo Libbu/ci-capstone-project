5c02bfaaaa16f0c837ba45217946703aebbe7078
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 14:11:34 +0000

    draft event submission form in events/forms.py

--- events/forms.py
+++ events/forms.py
@@ -9,4 +9,20 @@ class CreateEventForm(forms.ModelForm):
     """
     class Meta:
          model = Event
-         fields = ()
+         fields = [
+            'title',
+            'distance',
+            'organiser',
+            'description',
+            'image',
+            'event_date',
+            'event_time',
+            'location',
+            'max_attendees',
+         ]
+
+         widgets = {
+            'event_date': forms.DateInput(attrs={'type': 'date'}),
+            'event_time': forms.DateInput(attrs={'type': 'time'}),
+         }
+
