221a8794571a873159fcb9a705d74e5fca96aef3
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 15:56:39 +0000

    added events app url config to base.html navbar

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.EventList.as_view(), name='events')
+    path('', views.EventList.as_view(), name='event_list')
 ]
