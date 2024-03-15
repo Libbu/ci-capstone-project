1ac4f471bf693fb2d3a85c998acc1c2418478179
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 12:53:48 +0000

    preliminary MainAbout  model and home_ view function with docstrings describing intended behaviour of each

--- main/models.py
+++ main/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
 class MainAbout(models.Model):
-        
+    """
+    this model stores a single instance
+    of "about our community" text intended
+    to populate the corresponding area on 
+    the homepage
+    """    
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -3,3 +3,13 @@ from.models import MainAbout
 
 # Create your views here.
 def home_view(request):
+    """
+    this view returns the most recent instance
+    of :model: MainAbout to the 'about our community'
+    section of the homepage using the template main/index.html
+    """
+    home = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "main/index.html",{'home': home})
