b6a5cbbf9ea9df5f34c8003c38d65460f4c23be2
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 15:38:06 +0000

    docstring for describing EventList class view in events app

--- events/views.py
+++ events/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+
+class EventList(generic.ListView):
+    """
+    Returns all the approved events in
+    :model: `events.Event` and displays
+    them in a page of six posts
+    **Context**
+    
+    ``queryset``
+        All approved instances of :model: `events.Event`
+    ``paginate_by``
+        Number of events per page.
+    
+    **Template**
+
+    :template: `events/event_list.html`
+    """
