9522858d964bc36058e19d9475ba71505834e0f0
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 14:04:45 +0000

    added model fields for distance, event_date and event_time to event model, migrated db

--- events/models.py
+++ events/models.py
@@ -5,12 +5,15 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=200)
+    distance = models.CharField(max_length = 20)
     organiser = models.ForeignKey(User, on_delete=models.CASCADE, related_name="event_organiser")
     description = models.TextField()
     image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
+    event_date = models.DateField()
+    event_time = models.TimeField()
     location = models.CharField(max_length=200)
-    max_attendees = models.PositiveIntegerField('Maximum Attendees', default=0)
+    max_attendees = models.PositiveIntegerField('maximum_attendees', default=0)
     approved = models.BooleanField(default=False)
 
     class Meta:
