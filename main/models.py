10cc8321156a684f69a8b7a4ca3d2251cb8aa9eb
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 12:40:29 +0000

    beginning definition of MainAbout model and view to allow data-driven updates to main page by superusers

--- main/models.py
+++ main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MainAbout(models.Model):
+        
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from.models import MainAbout
 
 # Create your views here.
+def home_view(request):
