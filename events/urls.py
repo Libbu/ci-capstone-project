63116cbdc2a434d2d21d27532ededff5253f6d46
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-15 16:54:44 +0000

    connected event_detail.html and create_event.html to main navigation

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.EventList.as_view(), name='event_list'),
-    path('<int:event_id/', views.event_detail, name='event_detail')
+    path('create_event', views.create_event, name='create_event'),
+    path('event/<int:event_id/', views.event_detail, name='event_detail')
 ]
