434a9ce63b5d9a4459436c71d49445f478662737
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-20 15:33:20 +0000

    added delete confirmaiton modal to event_detail

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("btn-delete");
-const deleteConfirm = document.getElementById("deleteConfirm");
+
 
 /**
 * deletion functionality for delete confirmation button and modal.
@@ -8,8 +8,6 @@ const deleteConfirm = document.getElementById("deleteConfirm");
 
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
-      let eventId = e.target.getAttribute("event_id");
-      deleteConfirm.href = `event/${eventId}/delete/`;
       deleteModal.show();
     });
   }
