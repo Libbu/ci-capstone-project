b595055f25e1d51ce2d942a1a50824d5721b277c
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-23 17:18:50 +0000

    added JSDoc comments to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -9,8 +9,15 @@ const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_text");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
+
+
 /**
-* functionality for delete confirmation button and modal for Events.
+* Dunctionality for delete confirmation modal for Events.
+*
+* - listens for a click on the delete button in event_detail 
+* - and shows a modal with the option of deleting the event.
+*
+* - The deletion is handled by delete_event in events/views.py
 */
 
 for (let button of deleteButtons) {
@@ -19,8 +26,12 @@ for (let button of deleteButtons) {
     });
   }
 
-  /**
-* deletion functionality for delete confirmation button and modal for Comments.
+/**
+* Deletion functionality for delete button and modal for Comments.
+*
+* - retrieves associated comment ID when clicked
+* - ensures action mapped to delete comment url pattern
+* - displays modal to confirm user wishes to delete comment
 */
 
 for (let button of deleteButtonComment) {
@@ -31,6 +42,13 @@ for (let button of deleteButtonComment) {
   });
 }
 
+/* Edit functionality for edit button in comments.
+*
+* - retrieves associated comment ID when clicked
+* - populates CommentForm with conents of existing comment
+* - changes submit button text to read 'update'
+* - ensures action mapped to edit_comment url pattern
+*/
 
 for (let button of editButtons) {
   button.addEventListener("click", (e) => {
