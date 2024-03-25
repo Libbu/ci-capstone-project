03e79535163cc53792e402995771f926052985cd
Libbu Libbu <elizabeth.anne.spivey@gmail.com>
2024-03-25 00:20:35 +0000

    reduced size of hero image to improve lighthouse score

--- calm_cadence/settings.py
+++ calm_cadence/settings.py
@@ -36,7 +36,7 @@ import cloudinary.api
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io', 'https://*.herokuapp.com']
